39eae56801df0006d62057226c66a3317ca96912
DuGlaser <duglaser0618@gmail.com>
2020-04-25 23:24:51 +0900

    :sparkles:ADD the AddTodo component

---
+++ components/AddTodo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="add-todo">
+    <form @submit.prevent="onSubmit()">
+      <input type="text" v-model="title" />
+      <input type="submit" />
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'AddTodo',
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    ...mapActions(['createTodo']),
+    onSubmit() {
+      const newTodo = {
+        body: this.title,
+        isDone: false
+      }
+      this.createTodo(newTodo)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.add-todo {
+  margin-bottom: 2rem;
+}
+</style>
