0d698e2ec8809c04ba0e0bc6a43d5042c3d5c28e
DuGlaser <duglaser0618@gmail.com>
2020-04-25 21:41:43 +0900

    :construction:[WIP]ADD the TodoItem Component

---
+++ components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="checkbox" @click="onChange" />
+    {{ todo.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['todo'],
+  methods: {
+    onChange() {
+      console.log(this.todo.id)
+    }
+  }
+}
+</script>
+
+<style></style>
