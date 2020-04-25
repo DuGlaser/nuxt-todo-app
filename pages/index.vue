9594e20c355a565a04d30873a897cd58918b16cd
DuGlaser <duglaser0618@gmail.com>
2020-04-25 23:24:26 +0900

    :recycle:FIX pages/index

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,16 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-todo-app
-      </h1>
-      <h2 class="subtitle">
-        This is a simple todo app with nuxt.
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <AddTodo />
+    <Todos />
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Todos from '../components/Todos'
+import AddTodo from '../components/AddTodo'
 
 export default {
-  components: {
-    Logo
-  }
+  components: { Todos, AddTodo }
 }
 </script>
 
@@ -38,31 +18,10 @@ export default {
 .container {
   margin: 0 auto;
   min-height: 100vh;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  flex-direction: column;
 }
 </style>
