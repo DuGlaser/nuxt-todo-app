cc64e0960f5200417ce054dae0e2869fb0a5d21a
DuGlaser <duglaser0618@gmail.com>
2020-04-25 21:41:24 +0900

    :sparkles:ADD the Todos Component

---
+++ components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="todo in allTodos" :key="todo.id">
+      <TodoItem :todo="todo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import TodoItem from './TodoItem'
+
+export default {
+  name: 'Todos',
+  components: { TodoItem },
+  methods: {
+    ...mapActions()
+  },
+  computed: {
+    ...mapGetters(['allTodos'])
+  }
+}
+</script>
