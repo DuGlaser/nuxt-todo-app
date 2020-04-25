e90d9b351ad3b8d085fffc4ce5aab109569cd36e
DuGlaser <duglaser0618@gmail.com>
2020-04-25 23:23:10 +0900

    :sparkles:ADD some action to store/index

--- store/index.js
+++ store/index.js
@@ -1,19 +1,49 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-const state = {
-  todos: [
-    {
-      id: 1,
-      title: 'todo title',
-      isCompleted: false
-    }
-  ]
-}
+const BASE_URL = 'http://localhost:5000/todos'
+
+const state = () => ({
+  todos: []
+})
 const getters = {
   allTodos: (state) => state.todos
 }
-const actions = {}
-const mutations = {}
+const actions = {
+  async getTodos({ commit }) {
+    const res = await axios.get(BASE_URL)
+    console.log(res.data)
+    commit('setTodos', res.data)
+  },
+
+  async editTodo({ commit }, todo) {
+    try {
+      await axios.post(`${BASE_URL}/edit/${todo.id}`, {
+        body: todo.body,
+        isDone: todo.isDone
+      })
+
+      const res = await axios.get(BASE_URL)
+      commit('setTodos', res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  },
+
+  async createTodo({ commit }, todo) {
+    try {
+      await axios.post(BASE_URL, todo)
+      const res = await axios.get(BASE_URL)
+      commit('setTodos', res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+const mutations = {
+  setTodos: (state, todos) => {
+    state.todos = todos
+  }
+}
 
 export default {
   state,
