e094befc972e07d119e1ed7799275db9bd3dac24
DuGlaser <duglaser0618@gmail.com>
2020-04-25 23:23:52 +0900

    :sparkles:ADD the TodoItem's style

--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -1,20 +1,42 @@
 <template>
-  <div>
-    <input type="checkbox" @click="onChange" />
-    {{ todo.title }}
+  <div class="todo-item" @dblclick="handleMarked">
+    <span :class="{ 'is-done': todo.isDone }">{{ todo.body }}</span>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   name: 'TodoItem',
   props: ['todo'],
   methods: {
-    onChange() {
-      console.log(this.todo.id)
+    ...mapActions(['editTodo']),
+    handleMarked() {
+      const newTodo = {
+        id: this.todo.id,
+        body: this.todo.body,
+        isDone: !this.todo.isDone
+      }
+      console.log(newTodo)
+      this.editTodo(newTodo)
     }
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+.todo-item {
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  width: 100%;
+  padding: 1.5rem;
+  margin-bottom: 1rem;
+  border-left: #3e96d5 3px solid;
+  box-shadow: 1px 4px 4px #ccc;
+}
+.is-done {
+  text-decoration: line-through;
+}
+</style>
